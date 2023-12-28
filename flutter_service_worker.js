519a1e81cd679939962f4d4286df494f8741d08f
Evans Stenakis <ervans589@gmail.com>
2023-12-28 17:30:48 +0200

    Updated
    Adjusted layout and sizes for small screens.

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -132,9 +132,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "d5fb0952421a3b1f5853727a5a62aa38
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "740fc59f24573d931c3aae08bcccda69",
-"/": "740fc59f24573d931c3aae08bcccda69",
-"main.dart.js": "d909edb5f96b08ba0d1b0edb1318ae35",
+"index.html": "fbda5fc943140afa116331038db25156",
+"/": "fbda5fc943140afa116331038db25156",
+"main.dart.js": "51b8fc132121bf8ea00885d007dbd969",
 "manifest.json": "136936b7dd341c6433f45ef52be168ae",
 "version.json": "980547175e325fe622a3362b84d55b6a"};
 // The application shell files that are downloaded before a service worker can
